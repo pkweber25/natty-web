23bb47f7870a9820e5028ff19eda429900f5416e
pkweber25 <pkweber25@vt.edu>
2026-05-23 01:27:41 -0400

    only use react scan in dev

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Only load react-scan in development
+if (import.meta.env.DEV) {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/react-scan/dist/auto.global.js";
+  script.crossOrigin = "anonymous";
+  document.head.appendChild(script);
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
